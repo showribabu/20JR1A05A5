7c7020a408c31f363ffc56e165c5648eca91ce0b
showribabu <85867148+showribabu@users.noreply.github.com>
2023-08-02 12:29:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs=require("fs")
 const mysql=require("mysql2");
 const parser=require('body-parser');
 const url=require('url');
-
+const port = 3000;
 
 const app=express()
 
@@ -11,10 +11,48 @@ const app=express()
 //     res.end('Hello world');
 // })
 
+app.get('/numbers',function(req,res){
+    const urls = req.query.url;
+    if (!urls) {
+        return res.end('Not Avilable....!!');
+      }
+      const numbers = [];
+
+  try {
+    //data from the url...
+    const urlList = Array.isArray(urls) ? urls : [urls];
+
+    //lopp iteration
+    for (const url of urlList) {
+      const parsedUrl = new URL(url);
+
+      // Fetch the JSON data from the URL
+      const response = axios.get(parsedUrl.href);
+      const jsonData = response.data;
+
+      // Check if the response contains the 'numbers' array
+      if (Array.isArray(jsonData.numbers)) {
+        numbers.push(...jsonData.numbers);
+      }
+    }
+
+    return res.json({ numbers });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`number-management-service listening at http://localhost:${port}`);
+})
 
 
 
 app.listen(2000,function(req,res){
 
     console.log('server started');
+    
 })
+
+
